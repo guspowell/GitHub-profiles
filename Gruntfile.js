f4253fc4c78fd85d15775708e0084aa1ea35cb7b
guspowell <augustuspowell@me.com>
2015-03-26 11:10:01 +0000

    setup grunt to run server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,27 @@
 module.exports = function(grunt) {
 
- grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    mocha_casperjs: {
+grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+
+  mocha_casperjs: {
+    options: {},
+    files: {
+      src: ['test/**/*']
+    }
+  },
+
+  express: {
+    test: {
       options: {
-      },
-      files: {
-        src: ['test/**/*']
+        script: 'server.js'
       }
     }
-  })
+  },
+
+})
+
+  grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-mocha-casperjs');
 
-  grunt.registerTask('default', ['mocha_casperjs']);
+  grunt.registerTask('default', ['express:test', 'mocha_casperjs', 'express:test:stop']);
 };
